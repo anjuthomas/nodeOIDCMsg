9ae5f298299a6a0af6d665d1b51ae045c3e8f928
Anju Thomas <anjuthomas@anjuthomas-macbookpro.roam.corp.google.com>
2017-11-14 18:29:03 -0800

    Added more tests

--- LatestOicMsgAuth0-NodeJS/OicMsgAuth0-NodeJS/test/iat-test.js
+++ LatestOicMsgAuth0-NodeJS/OicMsgAuth0-NodeJS/test/iat-test.js
@@ -5,29 +5,29 @@ var assert = require('chai').assert;
 var BasicIdToken = require('../node_modules/tokenProfiles/basicIdToken');
 var decode = require('../node_modules/jsonwebtoken/decode');
 var jwt = require('../node_modules/jsonwebtoken');
-/*
+
 describe('iat', function () {
 
   it('should work with a exp calculated based on numeric iat', function (done) {
     var dateNow = Math.floor(Date.now() / 1000);
     var iat = dateNow - 30;
-    var expiresIn = 50;
-    var basicIdToken = new BasicIdToken('issuer','subject', 'audience', iat, iat + expiresIn, 10, "test");
-    var signedJWT = basicIdToken.toJWT('shhhh');
+    var expiresInVal = 50;
+    var basicIdToken = new BasicIdToken('issuer','subject', iat, "jti");
+    basicIdToken.addNonStandardClaims({"aud" : "audience"});
+    var signedJWT = basicIdToken.toJWT('shhhh', {expiresIn: expiresInVal});
 
     try{
-      var verificationClaims = {'iss': 'issuer','sub' : 'subject', 'aud' : 'audience', 'maxAge': '1d', 'clockTolerance' : 10, 'jti': 'test'};
+      var verificationClaims = {'iss': 'issuer','sub' : 'subject', 'aud' : 'audience', 'maxAge': '1d', 'jti': 'jti'};
       var decodedPayload = basicIdToken.fromJWT(signedJWT, 'shhhh', verificationClaims, {algorithms: ['HS256']});
     
       console.log(decodedPayload);
-      expect(decodedPayload.exp).to.be.closeTo(iat + expiresIn, 0.2);
+      expect(decodedPayload.exp).to.be.closeTo(iat + expiresInVal, 0.2);
       
     }catch(err){
-      console.log(err);
       assert.isNotNull(decodedPayload);
       assert.isNull(err);
     }
     done();
   });
-});*/
+});
 
